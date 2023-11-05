e467cbce9f27ec0206550d8c4360b9cdd63cf1fc
Eunice Mwenda <eunice.mwenda@student.moringaschool.com>
2023-11-05 15:12:08 +0300

    Mapping python classes to a database

--- lib/song.py
+++ lib/song.py
@@ -1,4 +1,37 @@
 from config import CONN, CURSOR
 
 class Song:
+    def __init__(self, name, album):
+        self.id = None
+        self.name = name
+        self.album = album
+
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS songs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                album TEXT
+            )
+        """
+        CURSOR.execute(sql)
+
+    def save(self):
+       sql = """
+        INSERT INTO songs (name, album)
+        VALUES (?, ?)
+    """
+       CURSOR.execute(sql, (self.name, self.album))
+       self.id = CURSOR.lastrowid 
+       CONN.commit()  
+
+
+
+    @classmethod
+    def create(cls, name, album):
+        song = Song(name, album)
+        song.save()
+        return song
+
     pass
